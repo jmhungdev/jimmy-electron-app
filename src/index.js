fc332b051fd1f056b9cd4309fd9a25a6f1b2b089
Hung, Jimmy <jmhungdev@gmail.com>
2024-09-27 00:57:38 -0500

    alter electron window size

--- src/index.js
+++ src/index.js
@@ -9,8 +9,8 @@ if (require('electron-squirrel-startup')) {
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 5000,
-    height: 800,
+    width: 800,
+    height: 600,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
